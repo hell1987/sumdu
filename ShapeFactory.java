e0b643ba3e9ae9619b42072f065e393062c6aaf5
hell1987 <72295530+hell1987@users.noreply.github.com>
2020-12-18 16:24:18 +0200

    Улучшение кода.
    ++++++

--- ShapeFactory.java
+++ ShapeFactory.java
@@ -3,6 +3,7 @@
  * @Author sumdu
  * @version 1.0.1
  */
+package lab3;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.GradientPaint;
@@ -13,95 +14,119 @@ import java.awt.geom.Arc2D;
 import java.awt.geom.GeneralPath;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
-    /**
+
+/**
      * ShapeFactory
      * начало обьявления нового класса ShapeFactory
      */
-public class ShapeFactory { 
+public class ShapeFactory
+{
     /**
      * 
      * Shape
      *  объявление метода фигуры
      */
-    public Shape shape; 
+    public Shape shape;
     /**
      * BasicStroke
      * объявление метода толщины линий
      */
 
-    public BasicStroke stroke = new BasicStroke(3.0f); 
+    public BasicStroke stroke;
     /**
      * класс Paint
      * объявление метода цвета фигуры
      */
-    public Paint paint ; 
+    public Paint paint;
     /**
      *
      * width
      * объявление метода с размером ширины фигуры
      */
-    public int width = 25;
-    
+    public int width;
     /**
      * height
      * объявление метода с размером высоты фигуры
      */
-    public int height = 25;
-/**
+    public int height;
+    
+    
+    
+    enum ShapeType {
+        SHEST,
+        STAR,
+        KVADR,
+        TREUG,
+        PACMAN
+    }
+    
+    enum ShapeVid {
+        TOL3,
+        NONE, 
+        TOL7,
+        GRADIENT,
+        RED
+    }
+    
+    /**
  * параметр переключения типа фигуры
  * @param shape_type параметр переключения типа фигуры
+ * @param shape_vid параметр переключения заливки, цвета и толщины
  * @throws Error сообщение что тип не поддерж
  * 
  */
-    public ShapeFactory(int shape_type) {
-        switch (shape_type / 8) { //переключение фигур 8/10/20
-            case 1: {
-                this.shape = ShapeFactory.createStar(3, new Point(0, 0), (double)this.width / 2.0, (double)this.width / 2.0);
+    public ShapeFactory(final ShapeType shape_type, final ShapeVid shape_vid) {
+        this.width = 25;
+        this.height = 25;
+        this.stroke = new BasicStroke(3.0f);
+        switch (shape_type) {
+            case SHEST: {
+                this.shape = createStar(3, new Point(0, 0), this.width / 2.0, this.width / 2.0);
                 break;
             }
-            case 3: {
-                this.shape = ShapeFactory.createStar(5, new Point(0, 0), (double)this.width / 2.0, (double)this.width / 4.0);
+            case STAR: {
+                this.shape = createStar(5, new Point(0, 0), this.width / 2.0, this.width / 4.0);
                 break;
             }
-            case 5: {
-                this.shape = new Rectangle2D.Double((double)(-this.width) / 2.0, (double)(-this.height) / 2.0, this.width, this.height);
+            case KVADR: {
+                this.shape = new Rectangle2D.Double(-this.width / 2.0, -this.height / 2.0, this.width, this.height);
                 break;
             }
-            case 7: {
-                GeneralPath path = new GeneralPath();
-                double tmp_height = Math.sqrt(2.0) / 2.0 * (double)this.height;
-                path.moveTo((double)(-this.width / 2), -tmp_height);
+            case TREUG: {
+                final GeneralPath path = new GeneralPath();
+                final double tmp_height = Math.sqrt(2.0) / 2.0 * this.height;
+                path.moveTo(-this.width / 2, -tmp_height);
                 path.lineTo(0.0, -tmp_height);
-                path.lineTo((double)(this.width / 2), tmp_height);
+                path.lineTo(this.width / 2, tmp_height);
                 path.closePath();
                 this.shape = path;
                 break;
             }
-            case 9: {
-                this.shape = new Arc2D.Double((double)(-this.width) / 2.0, (double)(-this.height) / 2.0, this.width, this.height, 30.0, 300.0, 2);
+            case PACMAN: {
+                this.shape = new Arc2D.Double(-this.width / 2.0, -this.height / 2.0, this.width, this.height, 30.0, 300.0, 2);
                 break;
             }
             default: {
                 throw new Error("type is nusupported");
             }
         }
-        switch (shape_type / 10) { //переключение цвета фигур и заливка градиентом
-            case 1: {
+        switch (shape_vid) {
+            case TOL3: {
                 this.stroke = new BasicStroke(3.0f);
                 break;
             }
-            case 3: {
+            case NONE: {
                 break;
             }
-            case 4: {
+            case TOL7: {
                 this.stroke = new BasicStroke(7.0f);
                 break;
             }
-            case 7: {
-                this.paint = new GradientPaint(-this.width, -this.height, Color.white, this.width, this.height, Color.gray, true);
+            case  GRADIENT: {
+                this.paint = new GradientPaint((float)(-this.width), (float)(-this.height), Color.white, (float)this.width, (float)this.height, Color.gray, true);
                 break;
             }
-            case 8: {
+            case RED: {
                 this.paint = Color.red;
                 break;
             }
@@ -110,28 +135,29 @@ public class ShapeFactory {
             }
         }
     }
- /**
-      * метод для создания звезды
-      * @param int arms количество краев
-      * @param Point center Центр звезды
-      * @param double rOuter Длина края
-      * @param double rInner Расстояние между центром и краем
+    
+    /**
+      * method for creating creating star
+      * @param int arms Arms count
+      * @param Point center Center of star
+      * @param double rOuter Length of edge
+      * @param double rInner Distance bettwen center and edge point
       * @return Shape 
     */  
-    private static Shape createStar(int arms, Point center, double rOuter, double rInner) {
-        double angle = Math.PI / (double)arms;
-        GeneralPath path = new GeneralPath();
+    private static Shape createStar(final int arms, final Point center, final double rOuter, final double rInner) {
+        final double angle = 3.141592653589793 / arms;
+        final GeneralPath path = new GeneralPath();
         for (int i = 0; i < 2 * arms; ++i) {
-            double r = (i & 1) == 0 ? rOuter : rInner;
-            Point2D.Double p = new Point2D.Double((double)center.x + Math.cos((double)i * angle) * r, (double)center.y + Math.sin((double)i * angle) * r);
+            final double r = ((i & 0x1) == 0x0) ? rOuter : rInner;
+            final Point2D.Double p = new Point2D.Double(center.x + Math.cos(i * angle) * r, center.y + Math.sin(i * angle) * r);
             if (i == 0) {
                 path.moveTo(p.getX(), p.getY());
-                continue;
             }
-            path.lineTo(p.getX(), p.getY());
+            else {
+                path.lineTo(p.getX(), p.getY());
+            }
         }
         path.closePath();
         return path;
     }
 }
-
